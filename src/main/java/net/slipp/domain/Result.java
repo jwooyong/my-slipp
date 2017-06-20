a34d955b3689832e3adc9c450c04febafcad4dd8
JWOOYONG\jwooyong <jwooyong@gmail.com>
2017-06-20 20:22:30 +0900

    exception 처리를 Result Class를 이용하여 처리토록 변경

---
+++ src/main/java/net/slipp/domain/Result.java
@@ -0,0 +1,32 @@
+package net.slipp.domain;
+
+public class Result {
+
+	private boolean valid;
+		
+	private String errorMessage;
+	
+	public Result(boolean valid, String errorMessage) {
+		
+		this.valid = valid;
+		this.errorMessage = errorMessage;		
+	}
+	
+	public static Result ok() {
+		return new Result(true, null);
+	}
+	
+	public static Result fail(String errorMessage) {
+		return new Result(false, errorMessage);
+	}
+	
+	public boolean isValid() {
+		return valid;
+	}
+
+	public Object getErrorMessage() {
+		// TODO Auto-generated method stub
+		return this.errorMessage;
+	}
+}
+
